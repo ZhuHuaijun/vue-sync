6be47b61103700c9782330c24fb1c7353afd4f18
hzhu1 <hzhu1@xiaohongshu.com>
2019-03-18 14:04:32 +0800

    update

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="container">
     <div @click="$sync(handleClick)">
       默认样式
     </div>
@@ -37,10 +37,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
+.container
   text-align center
   color #2c3e50
   height 200px
